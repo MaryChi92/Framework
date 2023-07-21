a27d9516e18db4c5f6b0330c1193c745423707cb
MaryChi92 <pereverzeva.mary@gmail.com>
2023-07-21 11:16:31 +0300

    lesson2 files added

--- mainapp/views.py
+++ mainapp/views.py
@@ -4,8 +4,13 @@ from framework_config.templator import render
 class TemplateView:
     template_name = "index.html"
 
-    def __call__(self, context):
-        return "200 OK", render(self.template_name, context=context)
+    def __call__(self, request):
+        if request["method"] == "GET":
+            return "200 OK", render(self.template_name, context=request)
+        elif request["method"] == "POST":
+            message = request["params"]
+            print(message)
+            return "200 OK", render(self.template_name, context=request)
 
 
 class IndexView(TemplateView):
